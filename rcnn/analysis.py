43256f610461107c40ad2b7e3ab3d45562f7db4f
shlomota <shlomotannor@mail.tau.ac.il>
2021-08-22 08:10:47 +0300

    added mist

--- rcnn/analysis.py
+++ rcnn/analysis.py
@@ -2,30 +2,78 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, precision_recall_curve, roc_auc_score, roc_curve, confusion_matrix, plot_confusion_matrix
 
-
+thresh = 1e-4
+thresh_clf = 0.2
 df = pd.read_csv("results.csv")
-df["score1"] = (df.score1>0.5).astype(np.int)
-
+df["score1"] = (df.score1>thresh).astype(np.int)
+df["mist"] = (df.mist>thresh).astype(np.int)
 df["score3"] = (df.score1) & (df.score2)
+df["score4"] = (df.score1) & (df.mist)
+df["score5"] = (df.score2) & (df.mist)
 
-df["bin_score"] = (df.score>0.5).astype(np.int)
+df["bin_score"] = (df.score>thresh_clf).astype(np.int)
 
 
 df1 = df[df["score1"]==0].sample(sum(df["score1"])).append(df[df["score1"]==1])
+df_mist = df[df["mist"]==0].sample(sum(df["mist"])).append(df[df["mist"]==1])
 df2 = df[df["score2"]==0].sample(sum(df["score2"])).append(df[df["score2"]==1])
+df3= df[df["score3"]==0].sample(sum(df["score3"])).append(df[df["score3"]==1])
+df4= df[df["score4"]==0].sample(sum(df["score4"])).append(df[df["score4"]==1])
+df5= df[df["score5"]==0].sample(sum(df["score5"])).append(df[df["score5"]==1])
 
-print("score1 results:")
+print("saint results:")
 print("Accuracy: %.3f" % accuracy_score(df1["score1"], df1["bin_score"]))
 print("Precision: %.3f" % precision_score(df1["score1"], df1["bin_score"]))
 print("Recall: %.3f" % recall_score(df1["score1"], df1["bin_score"]))
 print("ROCAUC: %.3f" % roc_auc_score(df1["score1"], df1["score"]))
+print("\n")
 
+print("mist results:")
+print("Accuracy: %.3f" % accuracy_score(df_mist["score1"], df_mist["bin_score"]))
+print("Precision: %.3f" % precision_score(df_mist["score1"], df_mist["bin_score"]))
+print("Recall: %.3f" % recall_score(df_mist["score1"], df_mist["bin_score"]))
+print("ROCAUC: %.3f" % roc_auc_score(df_mist["score1"], df_mist["score"]))
 
-print("\n\nscore2 results:")
+print("\n")
+print("table2 results:")
 print("Accuracy: %.3f" % accuracy_score(df2["score2"], df2["bin_score"]))
 print("Precision: %.3f" % precision_score(df2["score2"], df2["bin_score"]))
 print("Recall: %.3f" % recall_score(df2["score2"], df2["bin_score"]))
 print("ROCAUC: %.3f" % roc_auc_score(df2["score2"], df2["score"]))
+
+
+print("\n")
+print("score mist&saint results:")
+print("Accuracy: %.3f" % accuracy_score(df4["score4"], df4["bin_score"]))
+print("Precision: %.3f" % precision_score(df4["score4"], df4["bin_score"]))
+print("Recall: %.3f" % recall_score(df4["score4"], df4["bin_score"]))
+print("ROCAUC: %.3f" % roc_auc_score(df4["score4"], df4["score"]))
+
+
+print("\n")
+print("score mist&table2 results:")
+print("Accuracy: %.3f" % accuracy_score(df5["score5"], df5["bin_score"]))
+print("Precision: %.3f" % precision_score(df5["score5"], df5["bin_score"]))
+print("Recall: %.3f" % recall_score(df5["score5"], df5["bin_score"]))
+try:
+    print("ROCAUC: %.3f" % roc_auc_score(df5["score5"], df5["score"]))
+except:
+    pass
+
+print("\n")
+print("score saint&table2 results:")
+print("Accuracy: %.3f" % accuracy_score(df3["score3"], df3["bin_score"]))
+print("Precision: %.3f" % precision_score(df3["score3"], df3["bin_score"]))
+print("Recall: %.3f" % recall_score(df3["score3"], df3["bin_score"]))
+print("ROCAUC: %.3f" % roc_auc_score(df3["score3"], df3["score"]))
+
+print("length of mist&table2 dataset:", len(df5))
+print("length of mist&saint dataset:", len(df4))
+print("length of saint&table2 dataset:", len(df3))
+print("length of mist dataset:", len(df_mist))
+print("length of table2 dataset:", len(df2))
+print("length of saint dataset:", len(df1))
+
 """
 print("Accuracy:")
 print("Score1: %.3f" % accuracy_score(df["score1"], df["bin_score"]))
