6b2ccc28432b4d8eae1f374a306aaf0244b5cbb7
shlomota <shlomotannor@mail.tau.ac.il>
2021-08-29 22:50:09 +0300

    added remove_train_dups.py

--- data/remove_train_dups.py
+++ data/remove_train_dups.py
@@ -1,4 +1,5 @@
 import pandas as pd
 df = pd.read_csv("train_dup.txt", sep="\t")
 df = df.drop_duplicates(["Human Protein", "Virus Protein", "Species"])
+df.to_csv("train.txt", sep="\t", index=False)
 a = 5
