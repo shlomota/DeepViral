58cca84b36f1c01fa1eef49d0900c3c6aaedc65a
Noi Bar <noibarr@gmail.com>
2021-08-29 22:18:02 +0300

    fix is_in_range usage

--- rcnn/rcnn_covid_train_val.py
+++ rcnn/rcnn_covid_train_val.py
@@ -176,7 +176,7 @@ model = load_model(model_file)
 # Frozen the weights of the first CNN layers
 # frozen_layer_index = 20 # -5, -8, -12
 for i in model.layers:
-    if i in trainable_layers:
+    if is_in_range(trainable_layers, i):
         continue
     l = model.layers[i]
     l.trainable = False
