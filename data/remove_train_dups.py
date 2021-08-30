76baa59065fe8fbd277d65facffe343e82478f5e
shlomota <shlomotannor@mail.tau.ac.il>
2021-08-30 03:05:45 +0300

    updated train.txt

--- data/remove_train_dups.py
+++ data/remove_train_dups.py
@@ -1,5 +1,8 @@
 import pandas as pd
 df = pd.read_csv("train_dup.txt", sep="\t")
-df = df.drop_duplicates(["Human Protein", "Virus Protein", "Species"])
-df.to_csv("train.txt", sep="\t", index=False)
-a = 5
+df2 = df.dropna(subset=["MIscore"])
+df2 = df2.sort_values('MIscore', ascending=False).drop_duplicates(["Human Protein", "Virus Protein", "Species"]).sort_index()
+# df3 = df.drop_duplicates(["Human Protein", "Virus Protein", "Species"])
+df2.to_csv("train.txt", sep="\t", index=False)
+
+
